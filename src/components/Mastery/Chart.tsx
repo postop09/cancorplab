9614214bf479bc0826893cc2fddf136e9138d578
YUNSIK.CHO <93017923+postop09@users.noreply.github.com>
2023-05-02 18:16:45 +0900

    [마스터리차트] 버블차트 모듈화

--- src/components/Mastery/Chart.tsx
+++ src/components/Mastery/Chart.tsx
@@ -1,42 +1,65 @@
+// @ts-nocheck
 import React, { useEffect, useRef } from "react";
-import { range, scaleLinear, select, svg } from "d3";
 import { MasteryFullData } from "@/type/masteryData";
+import bubbleChart from "@/components/Mastery/bubbleChart";
 
 const Chart = ({ data }: { data: MasteryFullData[] }) => {
   const svgRef = useRef(null);
   console.log(data);
 
   useEffect(() => {
-    createChart();
+    if (data.length !== 0) {
+      createChart();
+    }
+    console.log(bubbleChart(data));
   }, [data]);
 
   const createChart = () => {
-    const SVG = select(svgRef.current);
-    const championPoints = data.map((item) => item.championPoints);
-    const I = range(championPoints.length).filter((i) => championPoints[i] > 0);
-    console.log(I);
+    // const SVG = select(svgRef.current);
+    // const championPoints = data.map((item) => item.championPoints);
+    // const I = range(championPoints.length).filter((i) => championPoints[i] > 0);
+    // console.log(I);
+    //
+    // SVG.selectAll("circle")
+    //   .data(data)
+    //   .join(
+    //     (enter) => enter.append("circle"),
+    //     (update) => update.attr("class", "updated"),
+    //     (exit) => exit.remove(),
+    //   )
+    //   .attr("id", "category_2")
+    //   .attr("r", 8)
+    //   .attr("cx", (v) => scaleLinear().domain())
+    //   .attr("cy", (v) => v.championPoints)
+    //   .attr("fill", "#3bbc9b")
+    //   .attr("fill-opacity", 0.1)
+    //   .attr("stroke", "#3bbc9b")
+    //   .attr("stroke-opacity", 0.5)
+    //   .attr("opacity", 1);
 
-    SVG.selectAll("circle")
-      .data(data)
-      .join(
-        (enter) => enter.append("circle"),
-        (update) => update.attr("class", "updated"),
-        (exit) => exit.remove(),
-      )
-      .attr("id", "category_2")
-      .attr("r", 8)
-      .attr("cx", (v) => scaleLinear().domain())
-      .attr("cy", (v) => v.championPoints)
-      .attr("fill", "#3bbc9b")
-      .attr("fill-opacity", 0.1)
-      .attr("stroke", "#3bbc9b")
-      .attr("stroke-opacity", 0.5)
-      .attr("opacity", 1);
+    const svg = bubbleChart(data, {
+      label: (d) => d.name,
+      title: (d) => d.name,
+      stroke: "#daa520",
+      strokeWidth: 1.5,
+      width: 1100,
+    });
+
+    if (!svgRef.current.childNodes.length) {
+      svgRef.current.append(svg);
+    }
   };
 
   return (
     <div>
-      <svg ref={svgRef}></svg>
+      <div ref={svgRef}></div>
+      {/*<>*/}
+      {/*  {bubbleChart(data, {*/}
+      {/*    label: (d) => d.name,*/}
+      {/*    title: (d) => d.name,*/}
+      {/*    width: 1100,*/}
+      {/*  })}*/}
+      {/*</>*/}
     </div>
   );
 };
--- src/const/API.ts
+++ src/const/API.ts
@@ -1,3 +1,3 @@
 export const RIOT_API_URL = "https://kr.api.riotgames.com/lol";
 
-export const RIOT_API_KEY = "RGAPI-6cc2ceb1-3f21-4c86-b769-3e6029c8a39c";
+export const RIOT_API_KEY = "RGAPI-3e266533-68ed-48c7-9a43-89bfd5e516e0";
