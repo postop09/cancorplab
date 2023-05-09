3045f0600829b4ab8f9861d0cc471f368dbeaf90
YUNSIK.CHO <93017923+postop09@users.noreply.github.com>
2023-05-09 21:38:23 +0900

    #1 [마스터리차트] API 키 최신화 후 테스트

--- src/const/API.ts
+++ src/const/API.ts
@@ -1,3 +1,3 @@
 export const RIOT_API_URL = "https://kr.api.riotgames.com/lol";
 
-export const RIOT_API_KEY = "RGAPI-f7293f48-8fd8-4b4e-b7a0-094db2c53649";
+export const RIOT_API_KEY = "RGAPI-096269e0-4c4a-445d-a4c0-61599fbe2631";
