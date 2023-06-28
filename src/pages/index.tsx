20516e5216f3b8825c1d2f2716b914bb3670e22b
YUNSIK.CHO <93017923+postop09@users.noreply.github.com>
2023-06-28 13:57:46 +0900

    #9 [테스트] api 폴더 미사용 파일 삭제, 스타일 경로 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Search from "@/components/Home/Search";
 import Title from "@/components/Home/Title";
 import Info from "@/components/Home/Info";
-import * as S from "./index.style";
+import styled from "styled-components";
 
 export default function Home() {
   return (
@@ -18,11 +18,35 @@ export default function Home() {
           crossOrigin="anonymous"
         ></script>
       </Head>
-      <S.Wrapper>
+      <Wrapper>
         <Title />
         <Search />
         <Info />
-      </S.Wrapper>
+      </Wrapper>
     </>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  max-width: ${({ theme }) => theme.WIDTH.pc};
+  min-height: 100vh;
+  margin: auto;
+  padding: 6rem;
+
+  &::after {
+    content: "";
+    background: url(/assets/img/champion/centered/Varus_7.jpg) no-repeat center center;
+    background-size: cover;
+    opacity: 0.5;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
+`;
