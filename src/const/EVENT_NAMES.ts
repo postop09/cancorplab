e6197b13461b9db205e0482654074209c29ab479
조윤식 <postop@tuneit.io>
2023-07-07 17:39:23 +0900

    #15 [리팩터링] 파이어베이스 추가

---
+++ src/const/EVENT_NAMES.ts
@@ -0,0 +1,13 @@
+export const EVENT_COMMON = {
+  share: "btn_share",
+  move: "btn_move_page",
+};
+
+export const EVENT_LBTI = {
+  page: "lbti",
+};
+
+export const EVENT_MASTERY = {
+  page: "mastery",
+  more: "btn_mastery_more",
+};
---
+++ src/lib/firebase.lib.ts
@@ -0,0 +1,34 @@
+// import firebase, { initializeApp } from "firebase/app";
+// import "firebase/analytics";
+// import { getAnalytics } from "@firebase/analytics";
+import firebase from "firebase/compat/app";
+import "firebase/compat/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfb_w8o9TAJIuiCBMZuVj_VFBwFfCTjzI",
+  authDomain: "canlab-ad204.firebaseapp.com",
+  projectId: "canlab-ad204",
+  storageBucket: "canlab-ad204.appspot.com",
+  messagingSenderId: "893247361569",
+  appId: "1:893247361569:web:e77ea2cba4e176467fc960",
+  measurementId: "G-HSMH9CLLJ9",
+};
+
+// 개발모드
+firebase.initializeApp(firebaseConfig);
+
+export const setFirebaseUserProperties = (
+  comName: string,
+  adminNo: number,
+  adminName: string,
+) => {
+  firebase.analytics().setUserProperties({
+    comName,
+    adminNo,
+    adminName,
+  });
+};
+
+export const analyticsLogEvent = (key: string, params?: {}) => {
+  firebase.analytics().logEvent(key, params);
+};
